2614a22e4c5ec567076fb2f9ca052cda805aef2a
AnandaSheva <sheva1257@gmail.com>
2024-11-22 22:49:16 +0700

    push code

---
+++ src/services/storeData.js
@@ -0,0 +1,10 @@
+const{firestore} = require('@google-cloud/firestore');
+ 
+async function storeData(id, data) {
+    const db = new Firestore();
+   
+    const predictCollection = db.collection('prediction');
+    return predictCollection.doc(id).set(data);
+  }
+   
+  module.exports = storeData;
